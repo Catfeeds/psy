b20dc0dc800d22a29d46b2fe1bc44d3d59d31658
ztenst <754710007@qq.com>
2018-04-18 12:41:19 +0800

    yohua

--- protected/modules/api/controllers/IndexController.php
+++ protected/modules/api/controllers/IndexController.php
@@ -231,7 +231,7 @@ class IndexController extends ApiController
             } elseif($form_id) {
                 $userit = UserExt::model()->findByPk($data['pid']);
                 $userut = UserExt::model()->findByPk($data['uid']);
-                $res = SmsExt::sendMsg('支付成功',$userut->phone,['name'=>$userut->name,'time'=>$data['begin']]);
+                $res = SmsExt::sendMsg('支付成功',$userit->phone,['name'=>$userut->name,'time'=>$data['begin']]);
                 Yii::log(json_encode($res));
                
             }
